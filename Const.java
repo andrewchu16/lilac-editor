1c099f9e78788bf0718cadbfebefa0fb1383d63e
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2023-05-27 23:13:37 -0400

    functional resizable window

--- Const.java
+++ Const.java
@@ -3,8 +3,10 @@ import java.awt.Point;
 
 public final class Const {
     public static final String SETTINGS_FILE_NAME = "settings.txt";
-    public static final String TITLE_NAME = "Lilac Editor";
 
+    // Window settings
+    public static final String TITLE_NAME = "Lilac Editor";
     public static final Dimension DEFAULT_DIMENSION = new Dimension(800, 800);
     public static final Point DEFAULT_LOCATION = new Point(100, 100);
+    public static final boolean DEFAULT_IS_MAXIMIZED = false;
 }
--- LilacEditor.java
+++ LilacEditor.java
@@ -12,24 +12,45 @@ public class LilacEditor {
         this.frame = new JFrame(Const.TITLE_NAME);
         this.settings = new Settings(Const.SETTINGS_FILE_NAME);
 
-        if (!this.settings.load()) {
-            this.frame.setLocation(Const.DEFAULT_LOCATION);
-            this.settings.setWindowDimension(Const.DEFAULT_DIMENSION);
-        }
-
-        this.frame.getContentPane().setPreferredSize(this.settings.getWindowDimension());
-        this.frame.setLocation(this.settings.getWindowLocation());
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setResizable(true);
 
+        this.loadSettings();
+    }
+
+    private void loadSettings() {
+        if (!this.settings.load()) {
+            this.settings.setWindowLocation(Const.DEFAULT_LOCATION);
+            this.settings.setWindowDimension(Const.DEFAULT_DIMENSION);
+            this.settings.setIsMaximized(Const.DEFAULT_IS_MAXIMIZED);
+        }
+        
+        this.frame.setLocation(this.settings.getWindowLocation());
+        this.frame.getContentPane().setPreferredSize(this.settings.getWindowDimension());
+
+        if (this.settings.getIsMaximized()) {
+            this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        }
+
         this.frame.addComponentListener(new ComponentAdapter() {
             public void componentMoved(ComponentEvent event) {
-                settings.setWindowLocation(frame.getLocation());
+                boolean isMaximized = (frame.getLocation().getX() < 0);
+                if (!isMaximized) {
+                    settings.setWindowLocation(frame.getLocation());
+                }
             }
 
             public void componentResized(ComponentEvent event) {
-                settings.setWindowDimension(frame.getSize());
-                // settings.setWindowDimension(getContentPane().getSize());
+                boolean isMaximized = (frame.getExtendedState() & JFrame.MAXIMIZED_BOTH) == JFrame.MAXIMIZED_BOTH;
+                if (!isMaximized) {
+                    if (settings.getIsMaximized()) {
+                        frame.pack();
+                    }
+                    settings.setWindowDimension(frame.getSize());
+                    // settings.setWindowDimension(getContentPane().getSize());
+                }
+
+                settings.setIsMaximized(isMaximized);
             }
         });
 
@@ -41,7 +62,9 @@ public class LilacEditor {
     }
 
     public void run() {
-        this.frame.pack();
+        if (!this.settings.getIsMaximized()) {
+            this.frame.pack();
+        }
         this.frame.setVisible(true);
     }
 
